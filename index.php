2065b51ee68d1851b81fbbb2ccc5c970b5567610
sekiikuya <seki.198.70@gmail.com>
2017-11-12 01:10:07 +0900

    フォルダ

---
+++ index.php
@@ -0,0 +1,131 @@
+<?php
+
+require_once("function.php");
+$errors = array();
+
+if(isset($_POST['submit'])){
+    
+    $name = $_POST['name'];
+    $memo = $_POST['memo'];
+
+    $name = htmlspecialchars($name, ENT_QUOTES);
+    $memo = htmlspecialchars($memo, ENT_QUOTES);
+
+    if($name === ''){
+        $errors['name'] = 'お名前が入力されていません。';
+    }
+
+    if($memo === ''){
+        $errors['memo'] = 'メモが入力されていません。';
+    }
+    
+    if(count($errors) === 0){
+        
+    	$dbh = db_connect();
+
+        $sql = 'INSERT INTO task (name, memo, done) VALUES (?, ?, 0)';
+        $stmt = $dbh->prepare($sql);
+
+        
+        $stmt->bindValue(1, $name, PDO::PARAM_STR);
+        $stmt->bindValue(2, $memo, PDO::PARAM_STR);
+
+        $stmt->execute();
+
+        $dbh = null;
+
+        unset($name, $memo);
+    }
+}
+
+if(isset($_POST['method']) && ($_POST['method'] === 'put')){
+    
+    $id = $_POST["id"];
+    $id = htmlspecialchars($id, ENT_QUOTES);
+    $id = (int)$id;
+
+    $dbh = db_connect();
+
+    $sql = 'UPDATE task SET done = 1  WHERE id = ?';
+    $stmt = $dbh->prepare($sql);
+    
+    
+    $stmt->bindValue(1, $id, PDO::PARAM_INT);
+    $stmt->execute();
+
+    $dbh = null;
+
+}
+
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Todoリスト</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<meta charset="utf-8">
+<title>Todoリスト</title>
+</head>
+<body>
+<h1>Todoリスト</h1>
+<?php
+if(isset($errors)){
+    print("<ul>");
+    foreach($errors as $value){
+        print("<li>");
+        print($value);
+        print("</li>");
+    }
+    print("</ul>");
+}
+?>
+<form action="index.php" method="post" id="form">
+<ul>
+    <li><span>タスク名</span><input type="text" name="name" value="<?php if (isset($name)){ print($name); } ?>"</li>
+    <li><span>メモ</span><textarea name="memo"><?php if(isset($memo)) { print($memo); } ?></textarea></li>
+    <li><input type="submit" name="submit"></li>
+</ul>
+</form>
+<?php
+	
+	$dbh = db_connect();
+
+	$sql = 'SELECT id, name, memo FROM task WHERE done = 0 ORDER BY id DESC';
+	$stmt = $dbh->prepare($sql);
+	$stmt->execute();
+	$dbh = null;
+
+	print('<dl>');
+	print('<div>');
+	while($task = $stmt->fetch(PDO::FETCH_ASSOC)){
+    	print('<div id="list">');
+	    //if ($task['done'] === '0') {
+		    print '<dt>';
+		    print $task["name"];
+		    print '</dt>';
+
+		    print '<dd>';
+		    print $task["memo"];
+		    print '</dd>';
+
+		    print '<dd>';
+		    print '
+		            <form action="index.php" method="post">
+		            <input type="hidden" name="method" value="put">
+		            <input type="hidden" name="id" value="' . $task['id'] . '">
+		            <button type="submit">済んだ</button>
+		            </form>
+		          ' ;
+		    print '</dd>';
+
+		    print '<hr>';
+		print('</div>');
+	//}
+}
+	print('</div)');
+	print('</dl>');
+?>
+</body>
+</html>
